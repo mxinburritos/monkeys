bf58177a6db2c78c316dbc8476debc651026812d
Edmund Xin <mxinburritos@gmail.com>
2020-05-06 12:29:22 -0700

    Updates

---
+++ precache-manifest.07d6e3000bf83a468faf1515f64cdd23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c5654a6122852fc484cfc67de1a126",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e531de7f9832692b4e4e",
+    "url": "/my-app/static/css/main.8f99eb00.chunk.css"
+  },
+  {
+    "revision": "112f2890872ef6dcfc0b",
+    "url": "/my-app/static/js/2.e3e343a3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-app/static/js/2.e3e343a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e531de7f9832692b4e4e",
+    "url": "/my-app/static/js/main.9eed3651.chunk.js"
+  },
+  {
+    "revision": "a826e09414c0bdbe3ee0",
+    "url": "/my-app/static/js/runtime-main.29edcf3d.js"
+  }
+]);
