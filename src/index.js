f7811d8bb027ae4cf6ca8d2728a0932d88996bbe
Edmund Xin <mxinburritos@gmail.com>
2020-05-07 16:14:23 -0700

    Dark mode set

--- src/index.js
+++ src/index.js
@@ -7,47 +7,7 @@ import purple from '@material-ui/core/colors/purple'
 
 const colorTheme = createMuiTheme({
   palette: {
-    primary: {
-      light: '#000',
-      main: '#000',
-      dark: '#000',
-      contrastText: '#000'
-    },
-    secondary: {
-      light: '#000',
-      main: '#000',
-      dark: '#000',
-      contrastText: '#000',
-    },
-    error: {
-      main: '#ff6666',
-      light: '#000',
-      dark: '#000',
-      contrastText: '#000',
-    },
-    divider: '#000',
-    action: {
-      active: '#444',
-      hover: '#444444',
-      selected: '#555',
-      disabled: '#000',
-      disabledBackground: '#000',
-    },
-    text: {
-      primary: '#fff',
-      secondary: '#ccc',
-      disabled: '#000',
-      hint: '#000',
-      icon: '#000',
-    },
-    common: {
-      black: '#000',
-      white: '#fff',
-    },
-    background: {
-      default: '#121212',
-      paper: '#303030',
-    },
+    type: 'dark',
   },
 });
 
