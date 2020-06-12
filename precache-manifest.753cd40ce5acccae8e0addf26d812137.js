5b6ed875cf09fddc99394a75b48515bfbbc49c4f
Edmund Xin <mxinburritos@gmail.com>
2020-06-12 14:13:16 -0700

    Updates

--- precache-manifest.753cd40ce5acccae8e0addf26d812137.js
+++ precache-manifest.753cd40ce5acccae8e0addf26d812137.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "acb0181a82160a44239bfd73b44734bf",
+    "revision": "fd6c5f075bd4e347472271d42728e5f3",
     "url": "/monkeys/index.html"
   },
   {
-    "revision": "2962f6d77913cd6ee02d",
+    "revision": "108348381728f2175521",
     "url": "/monkeys/static/css/main.bb96531b.chunk.css"
   },
   {
-    "revision": "051503341caa2eee79b7",
-    "url": "/monkeys/static/js/2.417d9e4c.chunk.js"
+    "revision": "c39730673e8ba6dc5705",
+    "url": "/monkeys/static/js/2.16edb45c.chunk.js"
   },
   {
     "revision": "44e39e61fb6a0450f7c8014318774752",
-    "url": "/monkeys/static/js/2.417d9e4c.chunk.js.LICENSE.txt"
+    "url": "/monkeys/static/js/2.16edb45c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2962f6d77913cd6ee02d",
-    "url": "/monkeys/static/js/main.e9786c0e.chunk.js"
+    "revision": "108348381728f2175521",
+    "url": "/monkeys/static/js/main.19fcea37.chunk.js"
   },
   {
     "revision": "545c84d5544ae5c1eec8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monkeys/precache-manifest.97796925fbfdce0433f106f91e2a5664.js"
+  "/monkeys/precache-manifest.753cd40ce5acccae8e0addf26d812137.js"
 );
 
 self.addEventListener('message', (event) => {
